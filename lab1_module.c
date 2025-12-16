4ccc2d0368e50209491bc70084a0b7bf76f1706f
0x00autistik_chan <104822608+oniichanwaoshimai@users.noreply.github.com>
2025-12-16 13:44:10 +0700

    Пробный модуль для загрузки в ядро Linux
    Лабараторная работа номер 3

--- lab1_module.c
+++ lab1_module.c
@@ -1,14 +1,19 @@
 #include <linux/kernel.h> /* необходим для pr_info() */
 #include <linux/module.h> /* необходим для всех модулей */
-int init_module(void)
-{
-pr_info("Welcome to the Tomsk State University\n");
+
+int hello_init(void) {
+	pr_info("Welcome to the Tomsk State University\n");
+	return 0;
+}
+
 /* Если вернётся не 0, значит, init_module провалилась; модули загрузить не
 получится. */
-return 0;
-}
-void cleanup_module(void)
+void hello_exit(void)
 {
 pr_info("Tomsk State University forever!\n");
 }
+
+module_init(hello_init);
+module_exit(hello_exit);
+
 MODULE_LICENSE("GPL");
